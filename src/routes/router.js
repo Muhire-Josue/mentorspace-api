46d8ae43b2f41134166f06c4d97caec66aac00fc
Muhire-Josue <rutayisire.josue09@gmail.com>
2019-08-26 13:08:24 +0000

    feature(view all mentors): Add view all mentors
    	- Added view all mentors
    
    [Finishes #167838476]

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import userController from '../controller/userController';
+import mentorController from '../controller/mentorController';
 import auth from '../middleware/auth';
 
 const router = express.Router();
@@ -8,6 +9,8 @@ const router = express.Router();
 router.post('/api/v1/auth/signup', userController.signUp);
 // Sign in
 router.post('/api/v1/auth/signin', userController.signIn);
+// Get all mentors
+router.get('/api/v1/auth/mentors', auth, mentorController.all);
 // Create session
 router.post('/api/v1/auth/sessions', auth, userController.createSession);
 
