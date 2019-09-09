0ee78b864b2eae0b8f7c0b8c4837be99e53a0195
Muhire-Josue <rutayisire.josue09@gmail.com>
2019-09-09 14:00:12 +0200

    feature(Sign in): add Sign in
    	- add sign in with database
    
    [Finishes #168374223]

--- server/tests/user.test.js
+++ server/tests/user.test.js
@@ -38,7 +38,6 @@ describe('User tests', () => {
         res.body.data.should.have.property('firstname');
         res.body.data.should.have.property('lastname');
         res.body.data.should.have.property('email');
-        res.body.data.should.have.property('password');
         res.body.data.should.have.property('address');
         res.body.data.should.have.property('bio');
         res.body.data.should.have.property('occupation');
@@ -140,6 +139,69 @@ describe('User tests', () => {
         res.body.should.be.an('object');
         res.body.status.should.be.equal(401);
         res.body.error.should.be.a('string');
+        done();
+      });
+  });
+  // Sign In
+  it('Should be login', (done) => {
+    const user = {
+      email: 'olubunmi@yaw.com',
+      password: 'user4',
+    };
+
+    chai.request(server)
+      .post('/api/v1/auth/signin')
+      .send(user)
+      .end((req, res) => {
+        res.body.status.should.be.equal(200);
+        res.body.should.be.an('object');
+        done();
+      });
+  });
+
+  it('Should not SignIn non-existing email', (done) => {
+    const user = {
+      email: 'muhirejosue@gmail.com',
+      password: 'user1',
+    };
+    chai.request(server)
+      .post('/api/v1/auth/signin')
+      .send(user)
+      .end((req, res) => {
+        res.body.status.should.be.equal(404);
+        done();
+      });
+  });
+
+
+  it('should not be able to signin when not signed up', (done) => {
+    const user = {
+      email: 'a@gmail.com',
+      password: 'bertin123',
+    };
+    chai.request(server)
+      .post('/api/v1/auth/signin')
+      .send(user)
+      .end((err, res) => {
+        res.body.status.should.be.equal(404);
+        res.body.should.be.an('object');
+        res.body.error.should.be.equal('user not found');
+        done();
+      });
+  });
+
+  it('should not be able to signin when passwords are not matching', (done) => {
+    const user = {
+      email: 'olubunmi@yaw.com',
+      password: 'trash',
+    };
+    chai.request(server)
+      .post('/api/v1/auth/signin')
+      .send(user)
+      .end((err, res) => {
+        res.body.status.should.be.equal(400);
+        res.body.should.be.an('object');
+        res.body.error.should.be.equal('password not matching');
         done();
       });
   });
