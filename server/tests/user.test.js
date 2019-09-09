68e2fc775b9c6f9c3b6777c2ed14cf97ba45733a
Muhire-Josue <rutayisire.josue09@gmail.com>
2019-09-09 13:27:32 +0200

    feature(Sign up): add Sign up
    	- add Sign up using database as storage
    
    [Finishes #168372531]

--- server/tests/user.test.js
+++ server/tests/user.test.js
@@ -1,15 +1,11 @@
-/* eslint-disable no-undef */
 import chai from 'chai';
 import chaiHttp from 'chai-http';
-import hash from 'bcrypt-nodejs';
 import server from '../../server';
 import db from '../model';
 
 chai.use(chaiHttp);
-const { expect } = chai;
 chai.should();
 
-// Sign Up
 describe('User tests', () => {
   // clear users table
   before(async () => {
@@ -21,7 +17,17 @@ describe('User tests', () => {
   });
 
   it('should be signup', (done) => {
-    const user = user0;
+    const user = {
+      firstname: 'Olubunmi',
+      lastname: 'Yaw',
+      email: 'olubunmi@yaw.com',
+      password: 'user4',
+      address: 'Gisenyi',
+      bio: 'HRmanager',
+      occupation: 'Human resources',
+      expertise: 'HR Manager',
+      status: 'user',
+    };
     chai.request(server)
       .post('/api/v1/auth/signup')
       .send(user)
@@ -31,7 +37,6 @@ describe('User tests', () => {
         res.body.data.should.have.property('firstname');
         res.body.data.should.have.property('lastname');
         res.body.data.should.have.property('email');
-        res.body.data.should.have.property('password');
         res.body.data.should.have.property('address');
         res.body.data.should.have.property('bio');
         res.body.data.should.have.property('occupation');
@@ -41,7 +46,16 @@ describe('User tests', () => {
       });
   });
   it('Should not work if all fields are not filled', (done) => {
-    const user = user02;
+    const user = {
+      firstname: 'Josue',
+      email: 'josue@gmail.com',
+      password: 'example12',
+      address: 'kigali-rwanda',
+      bio: 'DevOp manager',
+      occupation: 'software engineer',
+      expertise: 'backend engineer',
+      status: 'user',
+    };
     chai.request(server)
       .post('/api/v1/auth/signup')
       .send(user)
@@ -53,22 +67,18 @@ describe('User tests', () => {
       });
   });
 
-  it('Should not sign up existing email', (done) => {
-    const user = user03;
-
-    chai.request(server)
-      .post('/api/v1/auth/signup')
-      .send(user)
-      .end((req, res) => {
-        res.body.status.should.be.equal(401);
-        res.body.should.be.an('object');
-        res.body.error.should.be.a('string');
-        done();
-      });
-  });
-
   it('Should not accept incorrect email input format', (done) => {
-    const user = user04;
+    const user = {
+      firstname: 'Josue',
+      lastname: 'Muhire',
+      email: 123,
+      password: 'example12',
+      address: 'kigali-rwanda',
+      bio: 'DevOp manager',
+      occupation: 'software engineer',
+      expertise: 'backend engineer',
+      status: 'user',
+    };
 
     chai.request(server)
       .post('/api/v1/auth/signup')
@@ -105,67 +115,6 @@ describe('User tests', () => {
         res.body.should.be.an('object');
         res.body.status.should.be.equal(401);
         res.body.error.should.be.a('string');
-        done();
-      });
-  });
-  // Sign In
-  it('Should be login', (done) => {
-    const user = {
-      email: 'olubunmi@yaw.com',
-      password: 'user4',
-    };
-
-    chai.request(server)
-      .post('/api/v1/auth/signin')
-      .send(user)
-      .end((req, res) => {
-        res.body.status.should.be.equal(200);
-        res.body.should.be.an('object');
-<<<<<<< HEAD
-        expect(res.body.message).to.equal('User is successfully logged in');
-=======
-        done();
->>>>>>> chore(database): add Database
-      });
-  });
-
-  it('Should not SignIn non-existing email', (done) => {
-    const user = user07;
-    chai.request(server)
-      .post('/api/v1/auth/signin')
-      .send(user)
-      .end((req, res) => {
-        res.body.status.should.be.equal(404);
-        done();
-      });
-  });
-
-
-  it('should not be able to signin when not signed up', (done) => {
-    const user = user08;
-    chai.request(server)
-      .post('/api/v1/auth/signin')
-      .send(user)
-      .end((err, res) => {
-        res.body.status.should.be.equal(404);
-        res.body.should.be.an('object');
-        res.body.error.should.be.equal('user not found');
-        done();
-      });
-  });
-
-  it('should not be able to signin when passwords are not matching', (done) => {
-    const user = {
-      email: 'olubunmi@yaw.com',
-      password: 'trash',
-    };
-    chai.request(server)
-      .post('/api/v1/auth/signin')
-      .send(user)
-      .end((err, res) => {
-        res.body.status.should.be.equal(400);
-        res.body.should.be.an('object');
-        res.body.error.should.be.equal('password not matching');
         done();
       });
   });
