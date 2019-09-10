751a4b2cf01f01f323801fcf6817699992a681de
Muhire-Josue <rutayisire.josue09@gmail.com>
2019-09-10 09:29:59 +0200

    feature(view all mentors): add view mentors
    	- add view all mentors from the database
    
    [Finishes #168374892]

--- server/controller/mentorController.js
+++ server/controller/mentorController.js
@@ -1,130 +1,23 @@
-import Mentor from '../model/user';
-import Session from '../model/session';
-import sucess from '../helper/endPointResponse/sucess';
-import failure from '../helper/endPointResponse/failure';
-import newSession from '../helper/testObj/newSession';
-
+import db from '../model';
 
 class mentorController {
   // Get all Mentors
-  static all(req, res) {
-    const mentors = Mentor.filter(m => m.status === 'mentor');
+  static async all(req, res) {
+    const criterea = 'mentor';
+    const { rows } = await db.query('SELECT * FROM users WHERE status=$1', [criterea]);
+    const mentors = rows.map(function (item) {
+      delete item.password;
+      return item;
+    });
     if (req.user.status === 'user' || req.user.status === 'admin') {
-      const data = sucess('All mentors', 200, mentors);
-      res.status(data.status).json({
-        status: data.status,
-        message: data.message,
-        data: data.data,
+      res.status(200).json({
+        status: 200,
+        data: mentors,
       });
     } else {
-      const data = failure('Unauthorized access', 401);
-      res.status(data.status).json({
-        status: data.status,
-        error: data.message,
-      });
-    }
-  }
-
-  // Get a mentor
-  static findMentorById(req, res) {
-    const mentors = Mentor.filter(m => m.status === 'mentor');
-    if (req.user.status === 'user') {
-      const mentor = mentors.find(m => m.id === parseInt(req.params.id));
-      if (mentor) {
-        const data = sucess('Mentor profile', 200, mentor);
-        res.status(data.status).json({
-          status: data.status,
-          message: data.message,
-          data: data.data,
-        });
-      } else {
-        const data = failure('Mentor not found', 404);
-        res.status(404).json({
-          status: 404,
-          error: data.message,
-
-        });
-      }
-    } else {
-      const data = failure('Unauthorized access', 401);
-      res.status(data.status).json({
-        status: data.status,
-        error: data.message,
-      });
-    }
-  }
-
-
-  static acceptSession(req, res) {
-    if (req.user.status === 'mentor') {
-      const id = req.params.sessionId;
-      const session = Session.find(s => s.sessionId === parseInt(id));
-      if (session) {
-        
-        if (req.user.id === session.mentorId) {
-          session.status = 'accepted';
-          const data = sucess('Session accepted', 200, session);
-          res.status(data.status).json({
-            status: data.status,
-            message: data.message,
-            data: data.data,
-          });
-        } else {
-          const data = failure('Unauthorized access', 401);
-          res.status(data.status).json({
-            status: data.status,
-            error: data.message,
-          });
-        }
-      } else {
-        const data = failure('session not found', 404);
-        res.status(data.status).json({
-          status: data.status,
-          error: data.message,
-        });
-      }
-    } else {
-      const data = failure('Unauthorized access', 400);
-      res.status(data.status).json({
-        status: data.status,
-        error: data.message,
-      });
-    }
-  }
-
-
-  static rejectSession(req, res) {
-    if (req.user.status === 'mentor') {
-      const id = req.params.sessionId;
-      const session = Session.find(s => s.sessionId === parseInt(id));
-      if (session) {
-        if (req.user.id === session.mentorId) {
-          session.status = 'rejected';
-          const data = sucess('Session rejected', 200, session);
-          res.status(data.status).json({
-            status: 200,
-            message: data.message,
-            data: data.data,
-          });
-        } else {
-          const data = failure('Unauthorized access', 401);
-          res.status(data.status).json({
-            status: data.status,
-            error: data.message,
-          });
-        }
-      } else {
-        const data = failure('session not found', 404);
-        res.status(data.status).json({
-          status: data.status,
-          error: data.message,
-        });
-      }
-    } else {
-      const data = failure('Unauthorized access', 400);
-      res.status(data.status).json({
-        status: data.status,
-        error: data.message,
+      res.status(401).json({
+        status: 401,
+        error: 'Unauthorized access',
       });
     }
   }
